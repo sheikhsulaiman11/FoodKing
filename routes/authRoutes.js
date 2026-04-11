5009a5eaf5caf4b679b82a4700dca793dc8eb593
SULAIMAN <sheikhsulaimantahir@gmail.com>
2026-04-11 12:55:55 +0530

    added logout

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,6 +4,11 @@ import {signup, login} from '../controller/authController.js'
 
 router.post('/signup', signup)
 router.post('/login', login)
+router.get("/logout", (req, res) => {
+    res.clearCookie('token');
+    res.redirect('/login');
+});
+
 
 
 export default router;

c8b2885202c68dfe430962e7ceb11a44d19adcd5
SULAIMAN <sheikhsulaimantahir@gmail.com>
2026-04-13 09:36:23 +0530

    seperated backend and frontend

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 const router = express.Router();
-import {signup, login} from '../controller/authController.js'
+import {signup, login} from '../backend/controller/authController.js'
 
 router.post('/signup', signup)
 router.post('/login', login)

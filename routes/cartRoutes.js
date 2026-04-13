629de8c744a6a8970a4cdb8e8d2c7e5066c34e2a
SULAIMAN <sheikhsulaimantahir@gmail.com>
2026-04-13 06:57:36 +0530

    done with the cartRoutes

---
+++ routes/cartRoutes.js
@@ -0,0 +1,11 @@
+import express from 'express';
+const router = express.Router();
+import {getCart, addToCart, updateCart, removeFromCart} from '../controller/cartController.js';
+import { ProtectedRoute } from '../middleware/auth.js';
+
+router.get('/',ProtectedRoute, getCart);
+router.post('/add', ProtectedRoute,addToCart);
+router.patch('/update',ProtectedRoute, updateCart);
+router.delete('/remove/:productId',ProtectedRoute, removeFromCart);
+
+export default router;

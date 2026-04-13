9e7531100c17b84f09a006d57a82f58e2230e83e
SULAIMAN <sheikhsulaimantahir@gmail.com>
2026-04-13 06:45:12 +0530

    completed cartController.js

--- controller/cartController.js
+++ controller/cartController.js
@@ -30,3 +30,33 @@ export const addToCart = async (req, res) => {
         console.log(error);
     }
 }
+
+//update items in cart
+export const updateCart = async (req, res) => {
+    const {productId, quantity} = req.body;
+    try {
+        let cart = await Cart.findOne({user: req.user._id});
+        if(!cart) {return res.render('cart', {error: 'cart not found'})};
+        const item = cart.item.findOne(item => item.productId === Number(productId));
+        if(!item){return res.render('cart', {error: 'item not found'})};
+        item.quantity = Number(quantity);
+        await Cart.save();
+        res.redirect('/cart');
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+//remove item from cart
+export const removeFromCart = async (req, res) => {
+    const {productId} = req.params;
+    try {
+        let cart = await Cart.findOne({user: req.user._id});
+        if (!cart){return res.render('cart', {error: 'cart not found'})};
+        cart.items = cart.items.filter(item => item.productId !== Number(productId));
+        await cart.save();
+        res.redirect('/cart');
+    } catch (error) {
+        console.log(error);
+    }
+}

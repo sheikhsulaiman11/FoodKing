648265ee54ce312040dc4679d57a4ebaa760bcec
SULAIMAN <sheikhsulaimantahir@gmail.com>
2026-04-17 06:42:01 +0530

    added app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,36 @@
-import { useState } from 'react'
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Cart from './pages/Cart';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Restaurant from './pages/Restaurant';
+import NotFound from './pages/NotFound';
+import Navbar from './components/Navbar';
+import ProtectedRoute from './components/ProtectedRoute';
 
-function App() {
+const App = () => {
+    return (
+        <BrowserRouter>
+            <Navbar />
+            <Routes>
+                {/* Public routes */}
+                <Route path='/' element={<Home />} />
+                <Route path='/login' element={<Login />} />
+                <Route path='/register' element={<Register />} />
+                <Route path='/restaurant/:id' element={<Restaurant />} />
 
-  return (
-    <>
-      
-    </>
-  )
-}
+                {/* Protected routes */}
+                <Route path='/cart' element={
+                    <ProtectedRoute>
+                        <Cart />
+                    </ProtectedRoute>
+                } />
 
-export default App
+                {/* 404 */}
+                <Route path='*' element={<NotFound />} />
+            </Routes>
+        </BrowserRouter>
+    );
+};
+
+export default App;

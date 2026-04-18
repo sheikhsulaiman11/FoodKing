720a4e2c8ebe06ce7bd94a3add0d0c53ad3abe80
SULAIMAN <sheikhsulaimantahir@gmail.com>
2026-04-18 09:08:27 +0530

    added new files

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -1,7 +1,7 @@
 // pages/Login.jsx
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useAuth } from '../context/AuthContext';
+import { useAuth } from '../../context/AuthContext';
 
 const Login = () => {
     const [email, setEmail] = useState('');
